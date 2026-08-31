a0f21b0fd47561e0235b3d9677be337126bad21b
Lavanya Shyamsundar <lavanya10sn@gmail.com>
2026-08-30 23:14:33 -0700

    cleaning homepage ui

--- client/src/components/home/CourseCard.jsx
+++ client/src/components/home/CourseCard.jsx
@@ -4,7 +4,9 @@ export default function CourseCard({ course, onClick }) {
       <div className="course-card-heading">
         <h4>{course.course_name}</h4>
       </div>
-      <div className="panel-body" />
+      <div className="course-card-footer">
+        {course.assignment_count} assignment{course.assignment_count === 1 ? '' : 's'}
+      </div>
     </div>
   );
 }
